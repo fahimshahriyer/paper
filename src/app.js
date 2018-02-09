79418655a18fb07b90656e00b38c41341ba3a9d6
Fahim Shahriyer <shahriyers@gmail.com>
2018-02-10 01:32:25 +0600

    Load uikit icons

--- src/app.js
+++ src/app.js
@@ -2,3 +2,6 @@ import './assets/media/img/favicon.ico';
 import './assets/styles/app.scss';
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+
+// Loads the uikit icon plugin
+UIkit.use(Icons);
